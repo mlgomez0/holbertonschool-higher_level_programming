97dc757bd4bf4c7857e7017474919980b14b93f9
mlgomez0 <mlgomez230@hotmail.com>
2019-12-11 02:37:36 +0000

    First commit

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-flag = 0;
-for i in range(122, 98):
-    if flag == 0:
-        print("{:c}".format(i), end='')
+flag = 1;
+for i in range(122, 96, -1):
+    if flag == -1:
+        m = i - 32
+    else:
+        m = i
+    print("{:c}".format(m), end='')
+    flag = flag * (-1)
