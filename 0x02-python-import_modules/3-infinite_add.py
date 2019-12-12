341521cc297b39a4d9ec608e905d444959cd82c5
mlgomez0 <mlgomez230@hotmail.com>
2019-12-12 19:09:20 +0000

    First commit

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from sys import argv
+    a = 0
+    if len(argv) == 1:
+        print(0)
+    for i in range(1, len(argv)):
+        a = a + int(argv[i])
+    print(a)
