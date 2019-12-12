dfc9dc843650f5d74826064ab42ac4a15c37a73f
mlgomez0 <mlgomez230@hotmail.com>
2019-12-12 18:44:03 +0000

    First commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,9 @@
 if __name__ == '__main__':
     from sys import argv
     j = 1
-    if len(argv) == 2:
+    if len(argv) == 1:
+        print("{:d} arguments.".format(len(argv) - 1))
+    elif len(argv) == 2:
         print("{:d} argument:".format(len(argv) - 1))
     else:
         print("{:d} arguments:".format(len(argv) - 1))
