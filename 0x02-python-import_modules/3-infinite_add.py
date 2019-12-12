e48f395aade5749ed440cf2e8a9cd6cf786d1ae5
mlgomez0 <mlgomez230@hotmail.com>
2019-12-12 19:15:36 +0000

    First commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,6 +4,7 @@ if __name__ == '__main__':
     a = 0
     if len(argv) == 1:
         print(0)
-    for i in range(1, len(argv)):
-        a = a + int(argv[i])
-    print(a)
+    else:
+        for i in range(1, len(argv)):
+            a = a + int(argv[i])
+        print(a)
