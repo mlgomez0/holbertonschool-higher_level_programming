5906fdea0ad8c6d6ad2235bdf3891e876d185ad7
mlgomez0 <mlgomez230@hotmail.com>
2019-12-12 18:26:54 +0000

    First commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+if __name__ == '__main__':
+    from sys import argv
+    j = 1
+    if len(argv) == 1:
+        print("{:d} argument:".format(len(argv) - 1))
+    else:
+        print("{:d} arguments:".format(len(argv) - 1))
+    for i in range(1, len(argv)):
+        print("{:d}: ".format(j), end='')
+        j = j + 1
+        print(argv[i])
