b996cbb1932b650b19f92a064645ab4fc70231eb
mlgomez0 <mlgomez230@hotmail.com>
2019-12-13 00:10:04 +0000

    First commit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,10 +4,7 @@ if __name__ == '__main__':
     import calculator_1 as cal
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit (1)
-    elif argv[2] != "+" and argv[2] != "-" and argv[2] != "*" and argv[2] != "/":
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit (1)
+        exit(1)
     else:
         a = int(argv[1])
         b = int(argv[3])
@@ -20,6 +17,9 @@ if __name__ == '__main__':
         elif argv[2] == "*":
             c = cal.mul(a, b)
             print("{:d} * {:d} = {:d}".format(a, b, c))
-        else:
+        elif argv[2] == "/":
             c = cal.div(a, b)
-            print("{:d} / {:d} = {:d}".format(a, b,c))
+            print("{:d} / {:d} = {:d}".format(a, b, c))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
