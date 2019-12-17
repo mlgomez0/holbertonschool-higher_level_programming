d95b1d3726be1cb6ed66fb78e4fbe4aa1d7e022d
mlgomez0 <mlgomez230@hotmail.com>
2019-12-17 20:59:41 +0000

    First Commit

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence == "":
+        t = (0, None)
+        return (t)
+    else:
+        t = (len(sentence), sentence[0])
+        return (t)
+
