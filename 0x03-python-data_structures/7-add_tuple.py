6d95e3feb27d7895e25acf1da705e2afaa98cb0c
mlgomez0 <mlgomez230@hotmail.com>
2019-12-17 20:47:24 +0000

    First Commit

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) >= 2:
-        x, y = tuple_a
+        x = tuple_a[0]
+        y = tuple_a[1]
     elif len(tuple_a) == 1:
         x = tuple[0]
         y = 0
     else:
         x = y = 0
     if len(tuple_b) >= 2:
-        z, w = tuple_b
+        z = tuple_b[0]
+        w = tuple_b[1]
     elif len(tuple_b) == 1:
         z = tuple_b[0]
         w = 0
