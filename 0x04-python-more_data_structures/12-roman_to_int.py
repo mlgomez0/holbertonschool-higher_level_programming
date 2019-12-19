068bbef38362c8c9370a84f11a1c212fd659814c
mlgomez0 <mlgomez230@hotmail.com>
2019-12-19 22:55:43 +0000

    First commit

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string:
+        return (0)
+    sum_num = 0
+    tem = 0
+    dir_roman = {'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    for i in roman_string:
+        for k, v in dir_roman.items():
+            if i == k and (tem >= v or tem == 0):
+                sum_num += v
+                tem = v
+            elif i == k and tem < v:
+                sum_num -= v
+    return(abs(sum_num))
