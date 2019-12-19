5af308765be58a1c20ae289ce5acf0b7e0dbad96
mlgomez0 <mlgomez230@hotmail.com>
2019-12-19 18:54:44 +0000

    First commit

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dic = {k : 2 * v for (k, v) in a_dictionary.items()}
-    return (new_dic)
+    return({k: 2 * v for (k, v) in a_dictionary.items()})
