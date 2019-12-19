c63d38d4ab9f36b4bacb469cab79f7a0dad74400
mlgomez0 <mlgomez230@hotmail.com>
2019-12-19 20:08:24 +0000

    First commit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None:
         return (None)
-    my_list = sorted(a_dictionary.items(), key = lambda kv: (kv[1], kv[0]))
+    my_list = sorted(a_dictionary.items(), key=lambda kv: (kv[1], kv[0]))
     k, v = my_list[-1]
     return(k)
