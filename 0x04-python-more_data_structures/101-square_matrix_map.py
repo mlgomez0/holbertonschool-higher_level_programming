b78b859b5f3d90166fc79174403e3ac2c9f1fa80
mlgomez0 <mlgomez230@hotmail.com>
2019-12-20 23:02:06 +0000

    Fourth Commit

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    if not matrix:
-        return (None)
-
-    def squ_root(a_list=[]):
-        return([x * x for x in a_list])
-    return(list(map(squ_root, matrix)))
+    return(list(map(lambda x:  list(map(lambda x: x * x, x)), matrix)))
