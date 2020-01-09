34bd1f8056210561014c2f27615788d53ff48588
mlgomez0 <mlgomez230@hotmail.com>
2020-01-09 23:26:32 +0000

    First commit

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""This document defines the square class"""
-
 class Square:
-    def __init__(self, x = 0):
-        self.__size = x
+    def __init__(self, size=0):
+        if (type(size) != int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
