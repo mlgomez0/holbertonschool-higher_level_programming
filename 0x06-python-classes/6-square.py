979f570c974ff889994936f48c9db81e10c4f5cd
mlgomez0 <mlgomez230@hotmail.com>
2020-01-10 01:03:54 +0000

    First commit

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0, position=(0, 0)):
+        if (type(size) != int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+        if (type(position) != tuple or (len(position) != 2)
+			or type(position[0]) != int or type(position[1]) != int
+			or position[0] < 0 or position[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = position
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if (type(value) != int):
+            raise TypeError("size must be an integer")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if (type(value) != tuple or (len(value) != 2) or type(value[0]) != int
+			or type(value[1]) != int or value[0] < 0 or value[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def my_print(self):
+        if (self.__size != 0):
+            for i in range(self.__size):
+                print((self.__position[0] * " "), end='')
+                print(self.__size * "#")
+        else:
+            print("")
