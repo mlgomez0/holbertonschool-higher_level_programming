1d72235676822b5f1525fccdf4d4aefad1867cce
mlgomez0 <mlgomez230@hotmail.com>
2020-01-11 21:04:00 +0000

    First commit

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This module has one function add_integer"""
+
+
+def add_integer(a, b=98):
+
+    """
+    Function add integer:
+
+    Args:
+        a (int or float): The first parameter.
+        b (int or float): The second parameter.
+
+    Returns:
+        int: The return value is an integer addition of the two arguments
+    """
+
+    if ((type(a) != int and type(a) != float) or a is None):
+        raise TypeError("a must be an integer")
+    if ((type(b) != int and type(b) != float) or b is None):
+        raise TypeError("b must be an integer")
+    if (type(a) == float):
+        a = int(a)
+    if (type(b) == float):
+        b = int(b)
+    return a + b
