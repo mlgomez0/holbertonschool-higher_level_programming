8ef0f056bb720cbf4d11d7609f0d095e019100af
mlgomez0 <mlgomez230@hotmail.com>
2020-01-13 02:04:32 +0000

    101- matrix_mul

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""This module has one function lazy_matrix_mul"""
+import numpy as np
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    Function lazy_matrix_mul:
+
+    Args:
+        m_a (2D array): The first parameter.
+        m_b (2D array): The second parameter
+
+    Returns:
+        my_matrix: The return value is a new matrix
+    """
+    my_matrix = []
+    m = 0
+    la = len(m_a[0])
+    lb = len(m_a[0])
+    if (isinstance(m_a, list) is False):
+        raise TypeError("m_a must be a list")
+    if (isinstance(m_b, list) is False):
+        raise TypeError("m_b must be a list")
+    if (isinstance(m_a[0], list) is False):
+        raise TypeError("m_a must be a list of lists")
+    if (isinstance(m_b[0], list) is False):
+        raise TypeError("m_b must be a list of lists")
+    if (m_a == [] or m_a == [[]]):
+        raise ValueError("m_a can't be empty")
+    if (m_b == [] or m_b == [[]]):
+        raise TypeError("m_b can't be empty")
+    for i in range(len(m_a)):
+        if (len(m_a[i]) != len(m_b)):
+            raise ValueError("m_a and m_b can't be multiplied")
+        if (len(m_a[i]) != la):
+            raise TypeError("each row of m_a must be of the same size")
+        for j in range(len(m_b[0])):
+            if (len(m_b[j]) != lb):
+                raise TypeError("each row of m_b must be of the same size")
+            for z in range(len(m_b)):
+                if (type(m_a[i][z]) != int and type(m_a[i][z]) != float):
+                    raise TypeError("m_a should contain only integers or floats")
+                if (type(m_b[z][j]) != int and type(m_b[z][j]) != float):
+                    raise TypeError("m_b should contain only integers or floats")
+    my_matrix = np.dot(m_a, m_b)
+    return my_matrix
