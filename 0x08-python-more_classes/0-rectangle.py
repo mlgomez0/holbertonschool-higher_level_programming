10c281148abac65616e000ad4b8cd6efb9a18932
mlgomez0 <mlgomez230@hotmail.com>
2020-01-15 02:45:13 +0000

    Docstrings 0-

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""This module  has a class for rectangule instances.
+
+The module has the class Rectangle.
+"""
+
 
 class Rectangle:
+    """class rectangle.
+       the class pass
+
+    """
 
     def __init__(self):
         pass
--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -58,6 +58,7 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
+        return Rectangle(size, size)
 
     @property
     def height(self):
