979d2a8b8127620dca2108f8e695bacea9e68e7e
mlgomez0 <mlgomez230@hotmail.com>
2020-01-22 00:14:23 +0000

    sixth commit

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" My class Student
+"""
+
+
+class Student:
+    """ My class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        my_dic = {}
+        if attrs is None:
+            return self.__dict__
+        copy_dic = self.__dict__
+        for k, v in copy_dic.items():
+            if k in attrs:
+                my_dic[k] = v
+        return my_dic
