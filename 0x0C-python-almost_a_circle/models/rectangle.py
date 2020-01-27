dd8c77ffc0e8c9ae9ab14d5059286dbffe68def7
mlgomez0 <mlgomez230@hotmail.com>
2020-01-27 19:24:40 +0000

    docs correction

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-from models.base import Base
-
 """This module  has a Rectangle.
 """
+from models.base import Base
 
 
 class Rectangle(Base):
-
     """class Rectangle.
     the class defines the private instance attributes:
     __width, __height, __x, __y
@@ -14,6 +12,10 @@ class Rectangle(Base):
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """__init__
+        init method to create each instance
+
+        """
         if (type(width) != int):
             raise TypeError("width must be an integer")
         elif (width <= 0):
@@ -41,19 +43,31 @@ class Rectangle(Base):
         super().__init__(id)
 
     def area(self):
+        """
+        method to calculate the are of a rectangle
+        """
         return self.__width * self.__height
 
     def display(self):
+        """
+        method to display # signs
+        """
         s = "\n" * self.__y
         a = self.__width * "#" + "\n"
         b = " " * self.__x
         print(s + self.__height * (b + a), end="")
 
     def __str__(self):
+        """
+        method __str__ to format the print output
+        """
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.__x, self.__y, self.__width, self.__height)
 
     def update(self, *args, **kwargs):
+        """
+        method to update an instance attributes
+        """
         my_attris = ["id", "width", "height", "x", "y"]
         if args is not None and args != ():
             for i in range(len(my_attris)):
@@ -64,6 +78,10 @@ class Rectangle(Base):
                 setattr(self, key, value)
 
     def to_dictionary(self):
+        """
+        method to convert return instance attributes in
+        dictionary format
+        """
         my_copy = {}
         for k, v in self.__dict__.items():
             my_copy[k] = v
@@ -77,10 +95,16 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """
+        return the width attribute
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """
+        update the given attribute""
+        """
         if (type(value) != int):
             raise TypeError("width must be an integer")
         elif (value <= 0):
@@ -90,10 +114,16 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """
+        return the height
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """
+        update the height attribute
+        """
         if (type(value) != int):
             raise TypeError("height must be an integer")
         elif (value <= 0):
@@ -103,10 +133,16 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """
+        return the x attribute
+        """
         return self.__x
 
     @x.setter
     def x(self, value):
+        """
+        update the x attribute
+        """
         if (type(value) != int):
             raise TypeError("x must be an integer")
         elif (value < 0):
@@ -116,10 +152,16 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """
+        return the y attribute
+        """
         return self.__y
 
     @y.setter
     def y(self, value):
+        """
+        update the y attribute
+        """
         if (type(value) != int):
             raise TypeError("y must be an integer")
         elif (value < 0):
