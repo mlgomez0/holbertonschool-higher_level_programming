c34351887715755e2a8e0ad29b7e455217d3b9bf
mlgomez0 <mlgomez230@hotmail.com>
2020-03-03 21:39:20 +0000

    fix: db in command line

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- script that lists all the cities of California that can be found in the database hbtn_0d_usa.
 -- states table contains only one record where name = California, Results must be sorted in ascending order by cities.id.
-SELECT id, name from cities WHERE state_id = (SELECT id from states WHERE name = "California") ORDER BY id ASC;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY id ASC;
