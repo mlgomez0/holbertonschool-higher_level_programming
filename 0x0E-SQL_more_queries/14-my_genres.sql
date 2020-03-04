12929b1feab34a059751a530310aa776430fe886
mlgomez0 <mlgomez230@hotmail.com>
2020-03-04 04:13:48 +0000

    14- fix one select condition

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,6 +2,5 @@
 -- The tv_shows table contains only one record where title = Dexter (but the id can be different).
 SELECT tv_genres.name FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_genres.id = (SELECT tv_show_genres.genre_id WHERE tv_show_genres.show_id = (SELECT tv_shows.id WHERE tv_shows.title = "Dexter"))
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE tv_shows.title = "Dexter"
 ORDER BY tv_genres.name ASC;
