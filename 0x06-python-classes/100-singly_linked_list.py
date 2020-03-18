f6628646826e28ad4e8c078291f2a4e75222064e
mlgomez0 <mlgomez230@hotmail.com>
2020-03-18 23:42:07 +0000

    Fixed print

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -3,7 +3,7 @@ class Node:
     def __init__(self, data, next_node=None):
         if (type(data) != int):
             raise TypeError("data must be an integer")
-        elif (type(next_node) != Node and next_node != None):
+        elif (type(next_node) != Node and next_node is not None):
             raise TypeError("next_node must be a Node object")
         else:
             self.__data = data
@@ -26,10 +26,11 @@ class Node:
 
     @next_node.setter
     def next_node(self, value):
-        if (type(value) != Node and value != None):
+        if (type(value) != Node and value is not None):
             raise TypeError("next_node must be a Node object")
         else:
             self.__next_node = value
+
 
 class SinglyLinkedList:
     def __init__(self):
@@ -37,20 +38,31 @@ class SinglyLinkedList:
 
     def sorted_insert(self, value):
         NewNode = Node(value)
-        if (self.__head == None):
-            self.__head = Node(value)
+        if (self.__head is None):
+            self.__head = NewNode
             return self.__head
-        else:
-            temp = self.__head
-            while (temp.next_node):
-                temp = temp.next_node
-            temp.next_node = NewNode
+        temp = self.__head
+        if temp.data >= value:
+            self.__head = NewNode
+            self.__head.next_node = temp
             return self.__head
-    def lprint(self):
+        while (temp.next_node):
+            num = temp.next_node.data
+            if num >= value:
+                temp2 = temp.next_node
+                temp.next_node = NewNode
+                NewNode.next_node = temp2
+                return self.__head
+            temp = temp.next_node
+        temp.next_node = NewNode
+        return self.__head
+
+    def __str__(self):
         temp2 = self.__head
         while temp2 is not None:
-            print(temp2.__data)
+            if temp2.next_node is None:
+                print(temp2.data, end="")
+            else:
+                print(temp2.data)
             temp2 = temp2.next_node
-
-
-
+        return ""
