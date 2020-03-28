549baad978cbe652edf11f15fb648d100600ea7a
mlgomez0 <mlgomez230@hotmail.com>
2020-03-28 17:14:42 +0000

    1- remove Distinct

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,7 +5,7 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT DISTINCT id,\
+    cur.execute("SELECT id,\
                 name FROM states WHERE name RLIKE '^N.' ORDER BY id ASC")
     rows = cur.fetchall()
     for r in rows:
