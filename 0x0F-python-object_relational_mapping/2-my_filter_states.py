ceffc9124e3836d9babe59176f4ea8f7ffc252bf
mlgomez0 <mlgomez230@hotmail.com>
2020-03-28 04:58:15 +0000

    fix 2-

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""script to print name starting with N form a table using MySQLdb"""
+"""script to print name and id for given name for a table using MySQLdb"""
 import MySQLdb
 import sys
 if __name__ == "__main__":
-    state_in ="'" + sys.argv[4] + "'"
-    print(state_in)
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT DISTINCT id,\
-                name FROM states WHERE name = @state_in ORDER BY id ASC")
+                name FROM states WHERE name = %s\
+                ORDER BY id ASC", [sys.argv[4]])
 
     rows = cur.fetchall()
     for r in rows:
