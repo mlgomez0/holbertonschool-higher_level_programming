a70fad7b6526453c0e880df1f54c3fe1f0e2f651
mlgomez0 <mlgomez230@hotmail.com>
2020-03-28 01:55:51 +0000

    Create 0-

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+"""script to print elements in a table using MySQLdb"""
 import MySQLdb
 import sys
-db = MySQLdb.connect(user=sys.argv[1],passwd=sys.argv[2],db=sys.argv[3])
-cur = db.cursor()
-cur.execute("SELECT id, name FROM states")
-rows = cur.fetchall()
-for r in rows:
-    print(r)
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states")
+    rows = cur.fetchall()
+    for r in rows:
+        print(r)
