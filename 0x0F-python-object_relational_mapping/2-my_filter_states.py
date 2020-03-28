bd6479a2fdad59063357563c54df1f8fc33994f0
mlgomez0 <mlgomez230@hotmail.com>
2020-03-28 17:20:02 +0000

    fix 2 and 3

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""script to print name and id for given name for a table using MySQLdb"""
+"""print name and id for given name for a table using MySQLdb"""
 import MySQLdb
 import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT DISTINCT id,\
+    cur.execute("SELECT id,\
                 name FROM states WHERE name = '{}'\
                 ORDER BY id ASC".format(sys.argv[4]))
 
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""script to print name and id safety for given name for a table using MySQLdb"""
+"""print name and id safety for given name for a table using MySQLdb"""
 import MySQLdb
 import sys
 if __name__ == "__main__":
