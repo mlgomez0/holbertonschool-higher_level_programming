868ffa85d6d4508956456dd3d9ccf8a8cdf7b8e9
mlgomez0 <mlgomez230@hotmail.com>
2020-03-28 17:16:26 +0000

    1- remove Distinct

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT id,\
-                name FROM states WHERE name RLIKE '^N.' ORDER BY id ASC")
+                name FROM states WHERE name RLIKE '^[N].*$' ORDER BY id ASC")
     rows = cur.fetchall()
     for r in rows:
         print(r)
