52d281a9e513b8ced12978c85342736e30f95d07
mlgomez0 <mlgomez230@hotmail.com>
2020-03-28 19:43:57 +0000

    1- change search

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT states.id,\
-                states.name FROM states WHERE name RLIKE '^[N].*$'\
+                states.name FROM states WHERE name LIKE 'N%'\
                 ORDER BY states.id ASC")
     rows = cur.fetchall()
     for r in rows:
