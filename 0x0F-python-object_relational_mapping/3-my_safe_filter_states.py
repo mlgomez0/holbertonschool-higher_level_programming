2268bc20bc9b1bfb36d4e25132834741882e7432
mlgomez0 <mlgomez230@hotmail.com>
2020-03-28 12:44:03 +0000

    create 4-, create 3-, fix 2- by using format

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""script to print name and id for given name for a table using MySQLdb"""
+"""script to print name and id safety for given name for a table using MySQLdb"""
 import MySQLdb
 import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT DISTINCT id,\
-                name FROM states WHERE name = '{}'\
-                ORDER BY id ASC".format(sys.argv[4]))
+                name FROM states WHERE name = %s\
+                ORDER BY id ASC", [sys.argv[4]])
 
     rows = cur.fetchall()
     for r in rows:
