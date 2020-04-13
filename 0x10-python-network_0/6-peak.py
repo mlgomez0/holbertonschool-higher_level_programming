5fe060f5e5c5565185f520ba1aa7a33c2b7531dc
mlgomez0 <mlgomez230@hotmail.com>
2020-04-13 17:30:32 +0000

    6-fix using sorted

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,14 +4,8 @@
 
 def find_peak(list_of_integers):
     """returns one peak in a given array"""
-    i = 0
-    if list_of_integers is None or len(list_of_integers) == 0:
+    if len(list_of_integers) != 0 and list_of_integers is not None:
+        sorted_list = sorted(list_of_integers)
+        return sorted_list[-1]
+    else:
         return None
-    for i in range(len(list_of_integers)):
-        if i == 0 and list_of_integers[1] <= list_of_integers[0]:
-            return list_of_integers[0]
-        elif list_of_integers[i] >= list_of_integers[i + 1]\
-                and list_of_integers[i] >= list_of_integers[i - 1]:
-            return list_of_integers[i]
-        i += 1
-    return list_of_integers[i - 1]
