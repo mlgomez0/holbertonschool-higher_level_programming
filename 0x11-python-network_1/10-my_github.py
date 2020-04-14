9f82a2feb33f52356962e8edd9221da205f37468
mlgomez0 <mlgomez230@hotmail.com>
2020-04-14 22:56:59 +0000

    8-completed, 9-create

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# takes URL, sends a POST with variable q, displays [<id>] <name> or print error
+import requests
+import sys
+from requests.auth import HTTPBasicAuth
+
+user = sys.argv[1]
+passwd = sys.argv[2]
+try:
+    req = requests.get('https://api.github.com/user', auth=HTTPBasicAuth(user, passwd))
+    dir_element = req.json()
+    if len(dir_element) == 0:
+        print("No result")
+    else:
+        print(dir_element['id'])
+except:
+    print("None")
