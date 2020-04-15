6c190f4ff4ed10694602d6a5700dfeb0efff7ee5
mlgomez0 <mlgomez230@hotmail.com>
2020-04-15 18:19:40 +0000

    6- fixed comments

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-# takes URL, email and sends a POST request with email as a parameter
+"""takes URL, email and sends a POST request with email as a parameter"""
 
 import requests
 import sys
 
-url = sys.argv[1]
-email = sys.argv[2]
-req = requests.post(url, data={'email': email})
-print(req.text)
+if __name__ == "__main__":
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+    req = requests.post(url, data={'email': email})
+    print(req.text)
