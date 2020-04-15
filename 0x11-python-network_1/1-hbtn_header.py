d5d0cb222b83e01d1193b11327120e7f39a50c3b
mlgomez0 <mlgomez230@hotmail.com>
2020-04-15 17:54:24 +0000

    1- fixed comments

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-# takes and url sends a request and displays the value of the X-Request-Id.
+"""takes and url sends a request and displays the value of the X-Request-Id."""
 
 import urllib.request
 import sys
 
-url = sys.argv[1]
-with urllib.request.urlopen(url) as resp:
-    print("{}".format(resp.getheader('X-Request-Id')))
+if __name__ == "__main__":
+
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as resp:
+        print("{}".format(resp.getheader('X-Request-Id')))
