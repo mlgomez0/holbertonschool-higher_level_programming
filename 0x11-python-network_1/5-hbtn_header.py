a2b76408b2ee9998cebe07b419e2a6d83f099ce1
mlgomez0 <mlgomez230@hotmail.com>
2020-04-15 18:12:08 +0000

    5- fixed comments

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-# script that fetches https://intranet.hbtn.io/statu,  variable X-Request-Id
+"""script that fetches https://intranet.hbtn.io/statu,  variable X-Request-Id"""
 
 import requests
 import sys
 
-url = sys.argv[1]
-req = requests.get(url)
-print(req.headers['X-Request-Id'])
+if __name__ == "__main__":
+
+    url = sys.argv[1]
+    req = requests.get(url)
+    print(req.headers['X-Request-Id'])
