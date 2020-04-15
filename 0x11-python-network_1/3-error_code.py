790ea4cab181ee89b0526bea4bdcdb9dec694947
mlgomez0 <mlgomez230@hotmail.com>
2020-04-15 18:02:24 +0000

    3- fixed comments

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-# takes and url sends a request: displays body and handles error.
+"""takes and url sends a request: displays body and handles error"""
 
 import urllib.request
 import sys
 
-url = sys.argv[1]
-req = urllib.request.Request(url)
-try:
-    with urllib.request.urlopen(req) as resp:
-        print(resp.read().decode('utf-8'))
-except urllib.error.HTTPError as e:
-    print("Error code: {}".format(e.code))
+if __name__ == "__main__":
+
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as resp:
+            print(resp.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
