ef3bc4fd4bb6aabc575f0d68546d07c2b2569254
mlgomez0 <mlgomez230@hotmail.com>
2020-04-15 18:09:31 +0000

    4- fixed comments

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-# script that fetches https://intranet.hbtn.io/status using requests
+"""script that fetches https://intranet.hbtn.io/status using requests"""
 
 import requests
 
-req = requests.get('https://intranet.hbtn.io/status')
-print("Body response:")
-print("    - type: {}".format(type(req.text)))
-print("    - content: {}".format(req.text))
+if __name__ == "__main__":
+
+    req = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("	- type: {}".format(type(req.text)))
+    print("	- content: {}".format(req.text))
