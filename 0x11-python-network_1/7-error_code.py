17f72c55cc2771d3aea06aecc26dcfd9ac601a8a
mlgomez0 <mlgomez230@hotmail.com>
2020-04-15 18:21:46 +0000

    7- fixed comments

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-# takes URL, displays body of response and print error for status code >= 400
+"""displays body of URL response and print error for status code >= 400"""
 
 import requests
 import sys
 
-url = sys.argv[1]
-req = requests.get(url)
-if req.status_code < 400:
-    print(req.text)
-else:
-    print("Error code: {}".format(req.status_code))
+if __name__ == "__main__":
+
+    url = sys.argv[1]
+    req = requests.get(url)
+    if req.status_code < 400:
+        print(req.text)
+    else:
+        print("Error code: {}".format(req.status_code))
