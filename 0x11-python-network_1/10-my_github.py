2b157394f8a57e902b0efc0adf428f3e6e3fc109
mlgomez0 <mlgomez230@hotmail.com>
2020-04-15 18:28:16 +0000

    9- fixed comments

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-# takes URL, sends a POST with variable q, displays [<id>] <name> or print error
+"""takes URL, sends a POST with variable q, displays [<id>] <name> or print error"""
+
 import requests
 import sys
 from requests.auth import HTTPBasicAuth
 
-user = sys.argv[1]
-passwd = sys.argv[2]
-try:
-    req = requests.get('https://api.github.com/user', auth=HTTPBasicAuth(user, passwd))
-    dir_element = req.json()
-    print(dir_element['id'])
-except:
-    print("None")
+if __name__ == "__main__":
+
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    try:
+        req = requests.get('https://api.github.com/user', auth=HTTPBasicAuth(user, passwd))
+        dir_element = req.json()
+        print(dir_element['id'])
+    except:
+        print("None")
