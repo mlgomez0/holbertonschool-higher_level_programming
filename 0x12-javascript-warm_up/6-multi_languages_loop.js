1608b18979652039c7b566716d34d274c01e9e96
mlgomez0 <mlgomez230@hotmail.com>
2020-05-05 17:20:27 +0000

    create 6- file

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/* prints 3 lines, using array and loop */
+
+const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < 3; i++) {
+  console.log(arr[i]);
+}
