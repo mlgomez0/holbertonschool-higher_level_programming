8fe07adab01e9045e045d89cbb47c38cdb36cf5a
mlgomez0 <mlgomez230@hotmail.com>
2020-05-11 20:02:42 +0000

    create files

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*  prints the number of arguments already printed */
+
+let count = 0;
+
+exports.logMe = function (item) {
+  console.log(count + ':' + ' ' + item);
+  count = count + 1;
+};
