8be8a12e3c8f08f05648f73fab29eebc0aef9906
mlgomez0 <mlgomez230@hotmail.com>
2020-05-11 18:31:45 +0000

    create files

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+/*  class Rectangle that defines a rectangle + print method
+    rotate(): exchanges the width and the height of the rectangle
+    double(): multiples the width and the height of the rectangle by 2
+ */
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = 2 * this.width;
+    this.height = 2 * this.height;
+  }
+}
+module.exports = Rectangle;
