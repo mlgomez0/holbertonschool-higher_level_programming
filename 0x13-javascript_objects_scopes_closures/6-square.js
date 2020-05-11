e4cb95e9697c170ce27aa4ba27a57cf2c92cc909
mlgomez0 <mlgomez230@hotmail.com>
2020-05-11 19:12:23 +0000

    create files

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+/*  class Square that defines a square
+    and inherits from Square of 5-square.js
+ */
+
+const square = require('./5-square.js');
+
+class Square extends square {
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.width; i++) {
+        console.log(c.repeat(this.width));
+      }
+    } else {
+      this.print();
+    }
+  }
+}
+module.exports = Square;
