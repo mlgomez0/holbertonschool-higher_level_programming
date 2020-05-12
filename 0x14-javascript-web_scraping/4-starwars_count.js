adcbffd4e2bb8ef3e45ce63df57b1e0d5cdfaafd
mlgomez0 <mlgomez230@hotmail.com>
2020-05-12 16:43:33 +0000

    create files

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+/*  script that prints the number of movies where
+    the character "Wedge Antilles , ID 18" is present.
+ */
+
+const args = process.argv;
+const url = args[2];
+const request = require('request');
+let count = 0;
+request(url, function (error, response, body) {
+  if (error) {
+    return console.error(error);
+  }
+  const allTitles = JSON.parse(body);
+  for (let ti = 0; ti < allTitles.results.length; ti++) {
+    for (let chater = 0; chater < allTitles.results[ti].characters.length; chater++) {
+      if (allTitles.results[ti].characters[chater] === 'https://swapi-api.hbtn.io/api/people/18/') {
+        count = count + 1;
+      }
+    }
+  }
+  console.log(count);
+});
