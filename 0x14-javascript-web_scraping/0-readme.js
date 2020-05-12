6968553fd0aebbfaf0a03eb74135befae45696b0
mlgomez0 <mlgomez230@hotmail.com>
2020-05-12 15:27:41 +0000

    create files

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* script that reads and prints the content of a file */
+
+const args = process.argv;
+const file = args[2];
+const fs = require('fs');
+fs.readFile(file, 'utf-8', function (err, data) {
+  if (err) {
+    return console.error(err);
+  }
+  console.log(data);
+});
