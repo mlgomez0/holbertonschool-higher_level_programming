df8b8cdebf8abf8fd64f6542fa6b3ede650c1cb8
mlgomez0 <mlgomez230@hotmail.com>
2020-05-12 16:06:58 +0000

    create files

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*  script that prints the title of a Star Wars movie
+    where the episode number matches a given integer.
+ */
+
+const args = process.argv;
+const url = 'https://swapi-api.hbtn.io/api/films/' + args[2];
+const request = require('request');
+request(url, function (error, response, body) {
+  if (error) {
+    return console.error(error);
+  }
+  console.log(JSON.parse(body).title);
+});
