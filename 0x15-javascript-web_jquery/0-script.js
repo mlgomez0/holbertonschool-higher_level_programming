0a9da8080d46407e39838165ec96893007a44fe2
mlgomez0 <mlgomez230@hotmail.com>
2020-05-14 13:34:00 -0500

    add color change

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,9 +1,3 @@
-#!/usr/bin/node
+/* script that updates the text color of the HTML tag HEADER to red (#FF0000): */
 
-/* script that updates the text color of the HTML tag HEADER to red (#FF0000):*/
-
-#!/usr/bin/node
-
-/* script that updates the text color of the HTML tag HEADER to red (#FF0000):*/
-
-
+document.querySelector('header').style.color = '#FF0000';
